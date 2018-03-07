a93da497308d3a0c2c49f0c8285be232448fb063
hilihan <hiliguoxin@foxmail.com>
2018-03-07 15:28:22 +0800

    引入日志系统

--- src/main/java/cn/mark/o2o/web/superadmin/AreaController.java
+++ src/main/java/cn/mark/o2o/web/superadmin/AreaController.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,8 @@ import cn.mark.o2o.service.AreaService;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
+	
 	@Autowired
 	private AreaService areaService;
 
@@ -27,6 +32,8 @@ public class AreaController {
 	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<>();
 		List<Area> list = new ArrayList<>();
 		try {
@@ -38,6 +45,10 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime-startTime);
+		logger.info("===end===");
 		return modelMap;
 	}
 }
