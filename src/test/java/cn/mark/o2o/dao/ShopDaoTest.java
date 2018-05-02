28746d89aa777a1b79994a5e3cc1736c85fd19e1
hilihan <hiliguoxin@foxmail.com>
2018-05-02 19:09:38 +0800

    完成店铺列表页后端的开发

--- src/test/java/cn/mark/o2o/dao/ShopDaoTest.java
+++ src/test/java/cn/mark/o2o/dao/ShopDaoTest.java
@@ -87,21 +87,30 @@ public class ShopDaoTest extends BaseTest{
 	
 	@Test
 	public void testQueryShopList() {
+//		Shop shopCondition = new Shop();
+//		PersonInfo owner = new PersonInfo();
+//		owner.setUserId(9L);
+//		shopCondition.setOwner(owner);
+//		List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 10);
+//		System.out.println("店铺列表的大小：" + shopList.size());
+//		System.out.println("店铺列表的总数：" + shopDao.queryShopCount(shopCondition));
+		
+//		ShopCategory sc = new ShopCategory();
+//		sc.setShopCategoryId(36L);
+//		shopCondition.setShopCategory(sc);
+//		
+//		shopList = shopDao.queryShopList(shopCondition, 0, 2);
+//		System.out.println("xin店铺列表的大小：" + shopList.size());
+//		System.out.println("xin店铺列表的总数：" + shopDao.queryShopCount(shopCondition));
+		
 		Shop shopCondition = new Shop();
-		PersonInfo owner = new PersonInfo();
-		owner.setUserId(9L);
-		shopCondition.setOwner(owner);
-		List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 3);
+		ShopCategory childCategory = new ShopCategory();
+		ShopCategory parentCategory = new ShopCategory();
+		parentCategory.setShopCategoryId(36L);
+		childCategory.setParent(parentCategory);
+		shopCondition.setShopCategory(childCategory);
+		List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 10);
 		System.out.println("店铺列表的大小：" + shopList.size());
 		System.out.println("店铺列表的总数：" + shopDao.queryShopCount(shopCondition));
-		
-		ShopCategory sc = new ShopCategory();
-		sc.setShopCategoryId(36L);
-		shopCondition.setShopCategory(sc);
-		
-		shopList = shopDao.queryShopList(shopCondition, 0, 2);
-		System.out.println("xin店铺列表的大小：" + shopList.size());
-		System.out.println("xin店铺列表的总数：" + shopDao.queryShopCount(shopCondition));
-		
 	}
 }
